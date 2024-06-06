0cb65e0c075a42cd8ffc9cdbd89df5e2c1f3c952
RyuPrad <ryuprad@gmail.com>
2024-06-05 23:48:01 -0400

    Initial commit

---
+++ src/Student.jsx
@@ -0,0 +1,35 @@
+
+
+import propTypes from 'prop-types';
+
+function Student(props) {
+    return (
+        <div className="student">
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+            <p>Student: {props.isStudent ? "Yes" : "No"}</p>
+        </div>
+    );
+};
+
+// ? propTypes = a mechanism that ensures that the passed value
+// ?             is of the correct datatype.
+// ?             age: PropTypes.number
+
+Student.propTypes = {
+    name: propTypes.string,
+    age: propTypes.number,
+    isStudent: propTypes.bool,
+}
+
+// ? defaultProps = default values for props in case they are not
+// ?                passed from the parent component
+// ?                name: "Guest"
+
+Student.defaultProps = {
+    name: "Guest",
+    age: 0,
+    isStudent: false,
+}
+
+export default Student;
